ecc372cdcbfa55b2ded942aef4e09d3e738fa4bb
Randy DuCharme <radio.ad5gb@gmail.com>
2018-01-02 20:35:26 -0600

    Add GZOSP target
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,5 +15,6 @@
 #
 
 #add_lunch_combo tesla_deb-userdebug
-add_lunch_combo validus_deb-userdebug
+#add_lunch_combo validus_deb-userdebug
+add_lunch_combo gzosp_deb-userdebug
 #add_lunch_combo tipsy_deb-userdebug
